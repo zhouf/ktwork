6a589cc5408f267ca0ded870ccab3823fb2b868b
zhouf <zhouf_t@sohu.com>
2019-10-22 21:09:58 +0800

    modified

--- first.kt
+++ first.kt
@@ -3,9 +3,28 @@ import java.io.File
 fun main(args: Array<String>) {
     println("Hello from eclipse kotlin")
     //println("来一个中文试下")
-    print(foo(6))
-    foo()
+    //println(add(6,7))
+    //foo()
+    print(getstr2())
 }
+
+
+// 函数定义
+fun add(a:Int, b:Int):Int{
+    return a+b
+}
+fun foo(i:Int) = i*3
+
+
+fun myprint() = println("Hello from add function")
+
+public fun sum(a: Int, b: Int): Int = a + b   // public 方法则必须明确写出返回类型
+
+fun getstr():String{
+    return "OK from getstr()"
+}
+
+fun getstr2() ="hahahaha"
 
 fun foo(): Unit {
     var i=1
@@ -16,19 +35,8 @@ fun foo(): Unit {
     }
 }
 
-fun foo(i:Int) = i*3
-
+//如果返回Unit类型，可以省略
 fun listfolder(){
     val folders = File("C:\\").listFiles{file->file.isDirectory()}
     folders?.forEach{folder->println(folder)}
 }
-
-fun add(){
-    println("Hello from add function")
-}
-
-fun getstr():String{
-    return "OK from getstr()"
-}
-
-fun getstr2():String ="hahahaha"
